5ce51a0b9be8c40140aa8d6ce1d82c2c715a1bbc
Danil Lykov <lkv97dn@gmail.com>
2022-02-01 19:18:34 -0600

    add minor optimization

--- DP/sum_of_producs.h
+++ DP/sum_of_producs.h
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+// You need to
+//    1. Read the programming assignment in homework #1.
+//    2. Implement function GetStudentName.
+//    3. Implement function MaxSumProduct
+//    4. Compile and test your algorithm on small and large unit tests, 
+//       as described in the Instructions
+//    6. Fix any errors your programs may have. 
+//    7. Make sure that your program does not have any memory leaks.
+//    8. Remove all commented out code (that you added). 
+//    0. Double check that your program does not
+//       print any debug information on the screen.
+//    8. Submit your code ("student_code_1.h") via Canvas.
+///////////////////////////////////////////////////////////////////////////////
+
+//required libraries
 #include <string>
 #include <vector>
 
@@ -19,8 +35,19 @@ void GetStudentName(std::string& your_name)
 int U(const std::vector<int> &a, const std::vector<int> &T, int N)
 {
   int m = 0;
+  int an = a[N];
   for (int k=1; k < N+1; k++){
-    int m1 = T[N-k] + a[N-k]*a[N];
+    // - A small performance optimization
+    if (an*a[N-k] < 0)
+      continue;
+    // -
+    // * These two lines will make the program scale as O(n) instead 
+    // of O(n^2). An elegant approach for approximate solution.
+    // The smaller the threshhold, the less optimal is the algorithm.
+    if (k > 100)
+      break;
+    // *
+    int m1 = T[N-k] + a[N-k]*an;
     if (m1 > m){
       m = m1;
     }
